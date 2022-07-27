983c7208ce98b37ad655e918bee2b29c72050649
driquinha <adrienicassia@hotmail.com>
2022-07-27 15:22:09 -0300

    0001.2

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
 #! python3
+"""Hello Wold Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+"""
+__version__ = "0.0.1"
+__author__ = "dri"
+
 
 # teste
 print ('hello,  world!')
