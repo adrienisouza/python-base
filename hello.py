33e46f8e9658e4911e08eec76e11823a07dedd6f
driquinha <adrienicassia@hotmail.com>
2022-07-28 16:45:34 -0300

    Implementamo o Hello World multi linguas

--- hello.py
+++ hello.py
@@ -7,6 +7,18 @@ correspondente.
 __version__ = "0.0.1"
 __author__ = "dri"
 
+import os
 
 # teste
-print ('hello,  world!')
+
+current_language = os.getenv("LANG", "en_US") [:5]
+msg = "Hello, Wolrd!"
+
+if current_language == "pt_BR_":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+
+
+print (msg)
